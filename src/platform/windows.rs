ecaf4938fcfd22c5116224c950452f1cc3696fee
Johannes <jojurg@ttu.ee>
2025-12-27 22:08:41 +0200

    added empty windows implementation, project generation works but to be seen if it actually builds too

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,1 +1,26 @@
+use super::Platform;
+use crate::commands::dependencies::Dependency;
+use anyhow::Result;
+use std::process::Command;
 
+pub struct Windows;
+
+impl Platform for Windows {
+    fn name(&self) -> &'static str {
+        "Windows"
+    }
+
+    async fn check_dependencies(&self) -> anyhow::Result<Vec<Dependency>> {
+        let missing = Vec::<Dependency>::new();
+
+        Ok(missing)
+    }
+
+    async fn install_dependencies(&self, dependencies: &[Dependency]) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn install_instructions(&self, dependencies: &[Dependency]) -> String {
+        "".to_string()
+    }
+}
